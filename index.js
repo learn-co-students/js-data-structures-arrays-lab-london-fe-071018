bb2bf8c8a64bbc5a9f1dfac049d9debcd6442943
Halljamie18 <jamie.hall@blis.com>
2018-08-14 19:18:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver () {
+  drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver () {
+  drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver() {
+  return drivers.concat("Broom");
+}
+
+function prependDriver(name) {
+  return [name].concat(drivers);
+}
+
+function removeLastDriver() {
+  return drivers.slice(0, 2);
+}
+
+function removeFirstDriver() {
+  return drivers.slice(1);
+}
+
+// var newLength = fruits.push('Orange');
+
+
